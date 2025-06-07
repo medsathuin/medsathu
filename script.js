85846e78cbe9291ebdaf38c2aa46081ee20676d1
medsathuin <rathodsathunayak@gmail.com>
2025-06-07 22:18:25 +0530

    Update script.js
    Signed-off-by: medsathuin <rathodsathunayak@gmail.com>

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 function checkPasscode() {
-  const entered = document.getElementById("passcode").value;
-  const correct = "yourpasscode";  // Change this to your passcode
+  const entered = document.getElementById("rathodsathunayak@gmail.com").value;
+  const correct = "rathodsathunayak@gmail.com";  // Change this to your passcode
   if (entered === correct) {
     window.location.href = "tracker.html";
   } else {
